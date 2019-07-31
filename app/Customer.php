d61046ea639b4e3e6b91e1c66efe18c66aab0ad2
VACLOG - Alejandro Sagula <asagula@vaclog.com>
2019-07-31 14:10:39 -0300

    agregue veatify

--- app/Customer.php
+++ app/Customer.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     //
+
+    protected $fillable = ['codigo', 'nombre', 'calle', 'cuit', 'localidad', 'provincia', 
+    'disabled', 'client_id', 'audit_created_by', 'audit_updated_by'];
+
+
+    public function client(){
+        return $this->belongsTo('App\Client');
+    }
 }
